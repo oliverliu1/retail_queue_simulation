4f94f470be4c21082a93de5de1e7c4e7358ef62f
Oliver Liu <oliverliu100@gmail.com>
2021-07-08 11:03:04 -0400

    Remove redundant lines

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from cashier import Cashier
 from customer import Customer
 
 
+# Change create_env parameter default values to change the average times
 def create_env(number_of_cashiers, number_of_customers, mintime=60, maxtime=120, minspeed=1, maxspeed=3):
 
     cashier_instances = []
@@ -9,11 +10,9 @@ def create_env(number_of_cashiers, number_of_customers, mintime=60, maxtime=120,
 
     for i in range(number_of_cashiers):
         position = i + 1
-        mintime, maxtime = mintime, maxtime
         cashier_instances.append(Cashier(position, mintime, maxtime))
 
     for i in range(number_of_customers):
-        minspeed, maxspeed = minspeed, maxspeed
         customer_queue.append(Customer(minspeed, maxspeed))
 
     return cashier_instances, customer_queue
